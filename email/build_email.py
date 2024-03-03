bf48b493587b520fd9cbbe27511ac64c56153095
KrishnaSandeep79 <chittepusandeep1@gmail.com>
2024-03-03 01:12:25 -0700

    Calendar Invite

--- email/build_email.py
+++ email/build_email.py
@@ -1,15 +1,48 @@
+import datetime
 
-def build_email(user_name, event):
+def build_calendar_content(calendar_events):
+
+    for event in calendar_events:
+        event_details = f"""
+        <p><strong>{event['title']}</strong><br>
+        {event['description']}<br>
+        {event['url']}<br>
+        Start: {event['start_datetime'].strftime('%Y-%m-%d %H:%M')}<br>
+        End: {event['end_datetime'].strftime('%Y-%m-%d %H:%M')}</p>
+        """
+        html_body += event_details
+
+    ical_content = "BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//MyCal//App//EN\n"
+
+    for event in calendar_events:
+        ical_event = f"""BEGIN:VEVENT
+            UID:{datetime.datetime.now().strftime('%Y%m%dT%H%M%SZ')}@example.com
+            DTSTAMP:{datetime.datetime.now().strftime('%Y%m%dT%H%M%SZ')}
+            DTSTART:{event['start_datetime'].strftime('%Y%m%dT%H%M%SZ')}
+            DTEND:{event['end_datetime'].strftime('%Y%m%dT%H%M%SZ')}
+            SUMMARY:{event['title']}
+            DESCRIPTION:{event['description']}
+            END:VEVENT
+        """
+
+        ical_content += ical_event
+
+    ical_content += "END:VCALENDAR\n"
+
+    return ical_content
+
+def build_event(event, event_counter):
     
-    subject = None
-    body = None
+    cur_event = {
+        'summary': "Event " + event_counter,
+        'title': event['title'],
+        'description': event['description'],
+        'url': event['url'],
+        'start_datetime': event['startDate'],
+        'end_datetime': event['endDate']
+    }
 
-    if user_name is not None and event is not None:
+    return cur_event
 
-        subject = event['title'] + " event on " + event['startDate'][:10]
-        body = "Hi " + user_name + "," + " here\'s your personalized event,\n\n" + event['description'] + "\n\nEvent url:" + event['url']+ "\n\nThanks,\nTeam"
-
-    return [subject, body]
-
-# [subject, body] = build_email("Deveshwar", {'title': 'Connection', 'startDate': '2024-01-29T10:00:00-07:00', 'description': "Thanks"})
+# body = build_email("Deveshwar", {'title': 'Connection', 'startDate': '2024-01-29T10:00:00-07:00', 'description': "Thanks", 'url': 'url'})
 # print(body)
--- email/email_handler.py
+++ email/email_handler.py
@@ -1,7 +1,7 @@
 from fetch_user_data import fetch_user_data_from_dynamo
 from fetch_event_data import fetch_event_data_from_dynamo
 from send_email import send_email_to_user
-from build_email import build_email
+from build_email import build_event, build_calendar_content
 
 def email_handler(user_email, hash_id_list):
 
@@ -20,22 +20,30 @@ def email_handler(user_email, hash_id_list):
         events_list = fetch_event_data_from_dynamo(new_hash_ids)
 
         if len(events_list) > 0:
+            
+            calendar_events = []
+            email_content = None
+            event_counter = 1
+            subject = "Here are your personalized events!"
 
             for event in events_list:
-                
                 try:
-                    [subject, body] = build_email(user_data['userName'], event)
-                    if subject is not None and body is not None:
-                        send_email_to_user(user_data['emailId'], subject, body)
+                    calendar_events.append(build_event(event, event_counter))
                 except:
                     pass
 
-#email_handler("yrakeshchowdary1116@gmail.com", ['4635410de53a7cb35da2807e440f1732c37bd5726977889f4ad6f49cfc342fa8aa9235c69f343c711268f6f6bbb762508a8ed7809c359ca03aad40fe6cb6b765'])
+            email_content = build_calendar_content(calendar_events)
+            
+            # if email_content is not None:
+            #     send_email_to_user(user_data['emailId'], subject, email_content)
 
 
-
-
-
+hash_ids = [
+    "4635410de53a7cb35da2807e440f1732c37bd5726977889f4ad6f49cfc342fa8aa9235c69f343c711268f6f6bbb762508a8ed7809c359ca03aad40fe6cb6b765",
+    "25f5594a165187faed50d4e4d4ea80cb1daff929b5c6b80bbcd1fde60babf59185894c0b68270becceebd53e031d7f2536470c63ada93ead90add209bdd9e9a1",
+    "1acf8611825d9275c5971626e1de6645b72dafa949f3bd48c1ddfaf212c6fcdecacd57ab03ffa9e994e13855a183ccf321bee564787dda8f0f970450d6225329",
+]
+email_handler("krishnasandeep1825@gmail.com", hash_ids)
 
 
 
