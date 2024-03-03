ca4e7922c44b06aa7ae28084702592deb537cef4
KrishnaSandeep79 <chittepusandeep1@gmail.com>
2024-03-03 01:58:45 -0700

    New Date Changes

--- email/email_handler.py
+++ email/email_handler.py
@@ -37,12 +37,12 @@ def email_handler(user_email, hash_id_list):
                 send_email_to_user(user_data['emailId'], email_content, html_body)
 
 
-hash_ids = [
-    "4635410de53a7cb35da2807e440f1732c37bd5726977889f4ad6f49cfc342fa8aa9235c69f343c711268f6f6bbb762508a8ed7809c359ca03aad40fe6cb6b765",
-    "25f5594a165187faed50d4e4d4ea80cb1daff929b5c6b80bbcd1fde60babf59185894c0b68270becceebd53e031d7f2536470c63ada93ead90add209bdd9e9a1",
-    "1acf8611825d9275c5971626e1de6645b72dafa949f3bd48c1ddfaf212c6fcdecacd57ab03ffa9e994e13855a183ccf321bee564787dda8f0f970450d6225329",
-]
-email_handler("krishnasandeep1825@gmail.com", hash_ids)
+# hash_ids = [
+#     "4635410de53a7cb35da2807e440f1732c37bd5726977889f4ad6f49cfc342fa8aa9235c69f343c711268f6f6bbb762508a8ed7809c359ca03aad40fe6cb6b765",
+#     "25f5594a165187faed50d4e4d4ea80cb1daff929b5c6b80bbcd1fde60babf59185894c0b68270becceebd53e031d7f2536470c63ada93ead90add209bdd9e9a1",
+#     "1acf8611825d9275c5971626e1de6645b72dafa949f3bd48c1ddfaf212c6fcdecacd57ab03ffa9e994e13855a183ccf321bee564787dda8f0f970450d6225329",
+# ]
+# email_handler("krishnasandeep1825@gmail.com", hash_ids)
 
 
 
